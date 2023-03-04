1b5b73ac3c61172839f2da093115aea49bc2c253
Raahim <shraahim24@gmail.com>
2023-03-04 19:44:54 +0500

    Contact Section Created With Form Component Connected

---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import "./contact.css"
+import ContactForm from "../../components/contact-form/ContactForm"
+
+
+const Contact = () => {
+  return (
+    <div className="contact-section">
+      <ContactForm />
+    </div>
+  )
+}
+
+export default Contact
