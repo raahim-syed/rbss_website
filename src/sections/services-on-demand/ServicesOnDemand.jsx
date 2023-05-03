c5f4a3cb377f85a6d608ca9dac660112793ec1ac
Raahim <shraahim24@gmail.com>
2023-05-03 14:17:29 +0500

    Map function added in place of hardcoded data.

--- src/sections/services-on-demand/ServicesOnDemand.jsx
+++ src/sections/services-on-demand/ServicesOnDemand.jsx
@@ -2,53 +2,20 @@ import Heading from "../../components/heading/Heading"
 import LightCard from "../../components/light-card/LightCard"
 import "./services-on-demand.css"
 import background from "../../assets/images/contact-section-background.svg"
+import servicesOnDemand from "../../assets/data/services-on-demand.json"
 
 const ServicesOnDemand = ({extraClasses}) => {
   return (
     <div className={`services-on-demand ${extraClasses ? extraClasses : ""}`}>
         <div style={{textAlign: "center"}}>
-            <Heading mode="dark" subHeading="Services on demand" heading="On Demand Stuff" 
+            <Heading mode="dark" subHeading="Services on demand" heading="On Your Demands" 
                 paragraph="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, magni." />
                 
         </div>
-        <div className="grid grid-center container-sm mt-2">
-        <LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
+        <div className="grid container-sm mt-2" style={{justifyContent:"center", alignItems:"top"}}>
+                {servicesOnDemand.map((data, index) => <LightCard heading={data.heading} description={data.data} />)}
+                
 
-<LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
-
-<LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
-
-<LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
-
-<LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
-
-<LightCard heading="Provision of Guarding and Escorting Services through third party."
-         description="Lorem ipsum dolor sit amet, 
-         consetetur sadipscing elitr, sed diam 
-         nonumy eirmod tempor invidunt ut 
-         labore." />
-
-         
         </div>
     </div>
   )
