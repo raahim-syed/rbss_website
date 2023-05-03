535dd74666f167c0fcc9d5e71acd8ea01ad6d2ba
Raahim <shraahim24@gmail.com>
2023-05-03 14:16:32 +0500

    Footer styles added. 3 columns created with links and logo.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,33 @@
 import "./footer.css"
+import logo from "../../assets/images/logo.svg"
+import { Link } from "react-router-dom"
 
 const Footer = ({extraClasses, ...props}) => {
   return (
-    <footer className={`site-footer ${extraClasses} tops-rounded`} props >
-      <div className="container">
-        <h1>Footer</h1>
-      </div>  
+    <footer className={extraClasses ? `site-footer bg-dark tops-rounded ${extraClasses}` : `site-footer bg-dark tops-rounded`} props >
+      <section className="container grid grid-center">
+
+        <img src={logo} alt="rbss-logo" />
+
+        <div className="links">
+          <ul className="flex flex-center">
+            <li>
+              <Link to="/" >Home</Link>
+            </li>
+            <li>
+              <Link to="/about-consultant">About</Link>
+            </li>
+            <li>
+              <Link to="/privacy-policy">Privacy Policy</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="copyright">
+          <p>Copyright &copy; 2023, All rights reserved.</p>
+        </div>
+
+      </section>  
     </footer>
   )
 }
