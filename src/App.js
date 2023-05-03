7b6247d5700c4fe60dc21d4ec0e305e04ba08671
Raahim <shraahim24@gmail.com>
2023-05-03 14:15:58 +0500

    Privacy Policy page inserted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
-import AboutConsultant from './pages/AboutConsultant'
+import AboutConsultant from './pages/About/AboutConsultant'
 import Home from './pages/Home'
 import Layout from "./pages/layout/Layout"
+import PrivacyPolicy from './pages/Privacy-Policy/PrivacyPolicy'
 
 
 const router = createBrowserRouter(
@@ -10,6 +11,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="about-consultant" element={<AboutConsultant />} />
+            <Route path="privacy-policy" element={<PrivacyPolicy />} />
         </Route>
     )
 )
