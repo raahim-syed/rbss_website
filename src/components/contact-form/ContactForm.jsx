34ff68e617b07c2e75801cc4efe6c49f018bdf5a
Raahim <shraahim24@gmail.com>
2023-05-03 18:13:46 +0500

    EmailJs from integrated in contact us section.

--- src/components/contact-form/ContactForm.jsx
+++ src/components/contact-form/ContactForm.jsx
@@ -1,11 +1,30 @@
 import "./contact-form.css"
+import {phoneNumber} from "../../assets/data/basic-info.json";
 import {FaPhoneAlt} from  "react-icons/fa"
 import {FiMail} from "react-icons/fi"
 import Heading from '../heading/Heading'
 import FormControl from './form-control/FormControl'
 import ButtonPrimary from "../buttons/ButtonPrimary"
+import emailjs from '@emailjs/browser';
+import {useRef} from "react";
+
 
 const ContactForm = () => {
+    const form = useRef();
+
+    const sendEmail = async (e) => {
+        e.preventDefault();
+        
+        try{
+            const email = await emailjs.sendForm('service_lydidyo', 'template_5vmsa7j', form.current, 'd3pQmMd6quZzGZGs4');
+
+            console.log(email)
+        }catch(error){
+            console.log(error)
+        }  
+    };
+
+
   return (
     <div className="flex flex-center mt-4">
         <div className='contact-form-holder'>
@@ -13,7 +32,7 @@ const ContactForm = () => {
             <section className="contact-details mt-2 flex flex-space-between">
                 <span className="left flex flex-center">
                     <FaPhoneAlt />
-                    <span>03111-725848</span>
+                    <span>0311-725848</span>
                 </span>
                 <span className="right flex flex-center">
                     <FiMail />
@@ -21,12 +40,14 @@ const ContactForm = () => {
                 </span>
             </section>
 
-            <form action="">
-                <FormControl type="email" name="email" id="contact-id" placeholder="Enter Your Email" />
-                <FormControl type="name" name="subject" id="subject-id" placeholder="Enter Subject" />
-                <FormControl type="textarea" name="body" id="body-id" placeholder="Write..." rows="10" />
+            <form ref={form} onSubmit={sendEmail} action="/">
+                <FormControl type="text" name="from_name" id="contact-name" placeholder="Enter Your Name" />
+                <FormControl type="email" name="from_email" id="contact-id" placeholder="Enter Your Email" />
+                <FormControl type="text" name="subject" id="subject-id" placeholder="Enter Subject" />
+                <FormControl type="textarea" name="message" id="body-id" placeholder="Write..." rows="10" />
 
-                <ButtonPrimary type="dark" href="nowhere" text="Submit" />
+                <ButtonPrimary type="dark" text="Submit" />
+                {/* <button type="submit">Submit</button> */}
             </form>
         </div>       
     </div>
