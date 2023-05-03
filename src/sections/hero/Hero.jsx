ca5146980eb49249e140ef8b137e7239dd033a4c
Raahim <shraahim24@gmail.com>
2023-05-03 18:10:40 +0500

    Added hero section text in variables and removed redundant text.

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -4,20 +4,26 @@ import ButtonPrimary from "../../components/buttons/ButtonPrimary"
 import ButtonSecondary from "../../components/buttons/ButtonSecondary"
 
 const Hero = ({extraClasses, ...props}) => {
+const data = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed 
+              diam nonumy eirmod tempor invidunt ut labore et dolore 
+              magna aliquyam erat, sed diam voluptua. At vero eos et 
+              accusam et justo duo dolores et ea rebum. `
+
   return (
     <div className={`grid hero-section ${extraClasses ? extraClasses : ""}`} props>
       
       <section className="left">
         <Heading subHeading="Welcome" 
             heading="20+ Years Of Experience At Your Service"
-            paragraph="Some random paragrapgh" />
-
+            paragraph={data} />
+            
+          <div className="hero-buttons">
             <ButtonPrimary text="Contact Us" href="#contact" />
             <ButtonSecondary type="light" text="Learn More" href="/about-consultant" />
+          </div>
       </section>
 
       <section className="right">
-        Right
       </section>
     </div>
   )
