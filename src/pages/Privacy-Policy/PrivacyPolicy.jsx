821df602b675c7d607e39343c517d35b6b1c4590
Raahim <shraahim24@gmail.com>
2023-05-03 18:12:29 +0500

    Basic privacy policy component created.

---
+++ src/pages/Privacy-Policy/PrivacyPolicy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../../components/heading/Heading'
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <Heading mode='dark' subHeading="Privacy Policy" heading="Privacy Policy" bottom={false} />
+    </div>
+  )
+}
+
+export default PrivacyPolicy
